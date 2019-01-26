8ad4500ed3ebc6059fe2f1b1540061e5b6f6b9b9
Kevin Yuayan <yuayankevin@gmail.com>
2019-01-26 16:37:31 -0500

    added base template

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,8 @@
 /* Custom JavaScript goes Here */
+(function(){
+    function Start() {
+        console.log("%c App Started...", "font-size: 20px; color: blue; font-weight: bold");
+    }
+
+    window.addEventListener("load", Start);
+})
